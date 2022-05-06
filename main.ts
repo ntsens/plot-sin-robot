3a6fc6c4e3be02a7b5b6a80a1155d182b7d5fc01
ntsens <84364452+ntsens@users.noreply.github.com>
2022-05-06 18:28:20 +0800

    添加机器人画正弦曲线程序

--- main.ts
+++ main.ts
@@ -1,1 +1,37 @@
-
+/**
+ * 画正弦曲线
+ */
+let x = 0
+let y = 0
+let index = 0
+writingrobot.autoHome()
+let a = 20
+let n = 20
+let x_offset = 100
+let y_offset = 100
+// 画坐标轴
+// 画x轴
+writingrobot.moveXYZ(x_offset - 10, y_offset, -32)
+writingrobot.moveXYZ(x_offset + 50, y_offset, -32)
+// 抬笔
+writingrobot.moveXYZ(x_offset, y_offset, 0)
+// 画y轴
+writingrobot.moveXYZ(x_offset, y_offset + 30, -32)
+writingrobot.moveXYZ(x_offset, y_offset - 30, -32)
+// 抬笔
+writingrobot.moveXYZ(x_offset, y_offset, 0)
+while (index <= n) {
+    y = a * Math.sin(index / n * 2 * Math.PI)
+    x = 2 * index
+    x = x + x_offset
+    y = y + y_offset
+    basic.pause(300)
+    writingrobot.moveXYZunblock(x, y, -32)
+    index += 1
+}
+// 归位
+writingrobot.moveXYZ(x_offset, y_offset, 0)
+writingrobot.autoHome()
+basic.forever(function () {
+	
+})
